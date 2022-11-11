acd0d56bdf1696c6a21f6614d41c8a4054dc1d36
Matthew <mchen146@ucsc.edu>
2022-11-10 16:16:24 -0800

    modified navbar and feed

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,10 +9,11 @@ export default function RootLayout({ children }) {
         <html>
             <head>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto Serif" />
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto Serif&display=optional" />
             </head>
             <body>
                 <Navbar />
+                <div className='navbar-margin'></div>
                 {children}
             </body>
         </html>
